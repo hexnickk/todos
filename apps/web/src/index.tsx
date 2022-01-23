838481a3ac18e54718deb455a2b3967fd3a8cce1
Kozlov Nikita <kozlovzxc@gmail.com>
2022-01-23 18:17:12 +0300

    feat(common): added sentry & envs

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import { App } from './app.component';
+
+if (process.env.SENTRY_ENABLED === 'true') {
+    Sentry.init({
+        dsn: 'https://ab461c51338e4ba0b4a9b8967a89cca4@o1085243.ingest.sentry.io/6162956',
+        integrations: [new Integrations.BrowserTracing()],
+
+        // Set tracesSampleRate to 1.0 to capture 100%
+        // of transactions for performance monitoring.
+        // We recommend adjusting this value in production
+        tracesSampleRate: 1.0,
+    });
+}
 
 ReactDOM.render(
     <React.StrictMode>
