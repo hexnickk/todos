6ab7fb8ff2cc07c553c0c5588e75e6841c961a04
Kozlov Nikita <kozlovzxc@gmail.com>
2022-02-08 23:38:52 +0300

    feat(web): removed junk

--- apps/web/src/todos/stores/todos/init.ts
+++ apps/web/src/todos/stores/todos/init.ts
@@ -35,48 +35,6 @@ $todosList.on(todosNewSave, (list, newTodo) => {
     let todo = todoFromNew(newTodo);
     return [...list, todo];
 });
-$todosNew.on(todosNewSave, (_, newTodo) => null);
+$todosNew.on(todosNewSave, () => null);
 
-// $todosStore
-//     .on(todosCreateNew, (state, todo) => {
-//         if (todo == null) {
-//             let lastOrder = state.todos.reduce((acc, cur) => (cur.order > acc ? cur.order : acc), -1);
-//             return {
-//                 ...state,
-//                 newTodo: {
-//                     order: lastOrder,
-//                     completed: false,
-//                     title: '',
-//                 },
-//             };
-//         }
-//         return {
-//             ...state,
-//             todos: incOrder(state.todos, todo.order),
-//             newTodo: {
-//                 order: todo.order + 1,
-//                 completed: false,
-//                 title: '',
-//             },
-//         };
-//     })
-//     .on(todosCreate, (state, newTodo) => {
-//         let todo = todoFromNew(newTodo);
-//         return {
-//             ...state,
-//             todos: [...state.todos, todo],
-//             newTodo: null,
-//         };
-//     })
-//     .on(todosDelete, (state, deletedTodo) => ({
-//         ...state,
-//         todos: decOrder(
-//             state.todos.reduce((acc: Todo[], cur) => (cur.publicId === deletedTodo.publicId ? acc : [...acc, cur]), []),
-//             deletedTodo.order
-//         ),
-//     }))
-//     .on(todosDeleteNew, (state) => ({
-//         ...state,
-//         todos: state.newTodo ? decOrder(state.todos, state.newTodo?.order) : state.todos,
-//         newTodo: null,
-//     }));
+// $todosListSorted.watch((list) => console.log(JSON.stringify(list, null, 2)));
